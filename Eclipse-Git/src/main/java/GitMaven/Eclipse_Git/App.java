11df96db9183fa872a08a1ff3efaef6b4101c8b5
Aniketpatil352 <94597549+Aniketpatil352@users.noreply.github.com>
2024-07-05 12:10:46 +0530

    Initial commit

---
+++ Eclipse-Git/src/main/java/GitMaven/Eclipse_Git/App.java
@@ -0,0 +1,13 @@
+package GitMaven.Eclipse_Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
